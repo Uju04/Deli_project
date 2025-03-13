4fbce39cbd2f3ca9b5be9d8205a8d2b6a89eeafd
Oyidih Ezekiel Onuche <130329432+Manlikezeke@users.noreply.github.com>
2025-03-13 05:54:49 +0100

    deleted unnecessary files

--- Static/index.js
+++
@@ -1,6 +0,0 @@
-// home.js
-
-// Function to handle button click
-document.getElementById('exploreButton').addEventListener('click', function() {
-    window.location.href = 'item.html'; // Redirect to the items page
-});
--- Static/logout.js
+++
@@ -1,6 +0,0 @@
-// script.js
-
-// Function to redirect to the login page
-document.getElementById('loginAgainButton').addEventListener('click', function() {
-    window.location.href = 'login.html'; // Replace with your actual login page URL
-});
